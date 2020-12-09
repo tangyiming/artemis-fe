01983ed88d27aa4d86cbe65adc8b1da629534193
yiming.tang <yiming.tang@amh-group.com>
2020-12-09 10:03:46 +0800

    修复以在modifyVars中修改主题色

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Antd from 'ant-design-vue'
 import App from './App.vue'
-import 'ant-design-vue/dist/antd.css'
+import 'ant-design-vue/dist/antd.less'
 import './assets/styles/index.less'
 import router from './router'
 import store from './stores'
